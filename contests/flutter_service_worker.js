8fe6b45042c3d4292c84f11320089ab465f43d20
Signior-X <b19188@students.iitmandi.ac.in>
2020-10-30 23:27:19 +0530

    Added posters in events

--- contests/flutter_service_worker.js
+++ contests/flutter_service_worker.js
@@ -7,15 +7,15 @@ const RESOURCES = {
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/NOTICES": "681a28807206155ab80b31a0608f5aa2",
+"assets/NOTICES": "3d2039d72a3c662aa8b4affd2d9c0db6",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "ecell.png": "f1415aa5c86b3b9bf8b1d9d4a908aab4",
-"index.html": "df1ad11ae425ed244f24b9c513972194",
-"/": "df1ad11ae425ed244f24b9c513972194",
+"index.html": "2ab02ee09b04d58ef508eed6bcd23f61",
+"/": "2ab02ee09b04d58ef508eed6bcd23f61",
 "manifest.json": "231287ae28a683fae52e0a2509f2ef85",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"main.dart.js": "cfdace9770dc97d665cb18ded03c811c"
+"main.dart.js": "c607dc87acd0e67c7721d5ea773e1d0e"
 };
 
 // The application shell files that are downloaded before a service worker can
